85d0d13605287fd47cd847d357b25da2c7fa2925
alokcngh <alokcngh@gmail.com>
2025-05-01 16:45:38 +0530

    dev created B.java

--- GitProj01/src/main/java/com/alok/sevice/B.java
+++ GitProj01/src/main/java/com/alok/sevice/B.java
@@ -3,7 +3,7 @@ package com.alok.sevice;
 /**
  * Hello world!
  */
-public class App {
+public class B {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
