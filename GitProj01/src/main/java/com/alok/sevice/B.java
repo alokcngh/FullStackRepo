030e706e642fe5f574611cad808afab2c6e46bba
alokcngh <alokcngh@gmail.com>
2025-05-01 16:55:28 +0530

    dev making maodifiaction

--- GitProj01/src/main/java/com/alok/sevice/B.java
+++ GitProj01/src/main/java/com/alok/sevice/B.java
@@ -5,7 +5,7 @@ package com.alok.sevice;
  */
 public class B {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! dev modification");
         System.out.println("TL modification");
     }
 }
