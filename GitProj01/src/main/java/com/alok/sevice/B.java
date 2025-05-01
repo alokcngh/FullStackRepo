3b43a0d4f961e934e6f7ce5ce1a92cebd1079946
alokcngh <alokcngh@gmail.com>
2025-05-01 16:53:56 +0530

    TL is making modification at line no 7

--- GitProj01/src/main/java/com/alok/sevice/B.java
+++ GitProj01/src/main/java/com/alok/sevice/B.java
@@ -5,7 +5,7 @@ package com.alok.sevice;
  */
 public class B {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! TL modifiaction again");
         System.out.println("TL modification");
     }
 }
